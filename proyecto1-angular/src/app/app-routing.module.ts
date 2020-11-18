fc9472fffa3f4331383469ee14419618b8c52faf
FranklinPerez <franklin9perez952012@gmail.com>
2020-11-17 19:47:50 -0600

    CRUD usuarios para tomar de base

---
+++ proyecto1-angular/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule  } from '@angular/router';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { AppComponent } from './app.component';
+
+
+const routes: Routes = [
+    { path: 'usuarios', component: UsuarioComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- proyecto1-angular/src/app/app.module.ts
+++ proyecto1-angular/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { UsuarioService } from './usuario/usuario.service';
+import { FormsModule} from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [UsuarioService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
